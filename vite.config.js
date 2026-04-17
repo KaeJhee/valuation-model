09b360290aafb3b9bc79f65bfb30d1e47326b29f
User <krisgallow918@gmail.com>
2026-04-17 01:32:33 -0500

    Fix base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Change 'gs-valuation-model' to your actual GitHub repo name
-  // If deploying to https://username.github.io/gs-valuation-model/
-  // then base should be '/gs-valuation-model/'
+  // IMPORTANT: Change 'valuation-model' to your actual GitHub repo name
+  // If deploying to https://username.github.io/valuation-model/
+  // then base should be '/valuation-model/'
   // If deploying to a custom domain, set base to '/'
-  base: '/gs-valuation-model/',
+  base: '/valuation-model/',
 })
